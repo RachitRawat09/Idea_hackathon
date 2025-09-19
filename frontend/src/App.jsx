5aba63efa463b77c06f8d9520f429e9252e1e905
sakshi <sakhsishar8054@gmail.com>
2025-09-19 10:45:37 +0530

    chnages in home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout.jsx';
-import Landing from './pages/Landing.jsx';
+import Landing from "./pages/landing/Landing.jsx";
 import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
 import Dashboard from './pages/Dashboard.jsx';
 import Browse from './pages/Browse.jsx';
-import ListingCard from './components/ListingCard.jsx';
 import Messages from './pages/Messages.jsx';
 import ProductDetail from './pages/ProductDetail.jsx';
 import Profile from './pages/Profile.jsx';
@@ -18,18 +17,38 @@ import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
   return (
     <BrowserRouter>
-      <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} newestOnTop closeOnClick pauseOnFocusLoss draggable pauseOnHover />
+      {/* 🔹 Toast Notifications */}
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+
+      {/* 🔹 Routes */}
       <Routes>
+        {/* Layout will always include Header + Footer */}
         <Route path="/" element={<Layout />}>
+          {/* Landing page (default / route) */}
           <Route index element={<Landing />} />
+
+          {/* Auth pages */}
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
+
+          {/* User dashboard & features */}
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="browse" element={<Browse />} />
           <Route path="create-listing" element={<CreateListing />} />
           <Route path="messages" element={<Messages />} />
           <Route path="product/:id" element={<ProductDetail />} />
           <Route path="profile" element={<Profile />} />
+
+          {/* Admin */}
           <Route path="admin" element={<AdminDashboard />} />
         </Route>
       </Routes>
